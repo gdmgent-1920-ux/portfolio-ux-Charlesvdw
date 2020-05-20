6d7867e011d52ea4d11a3331e9c97607e1b319e1
Charlesvdw <61147928+Charlesvdw@users.noreply.github.com>
2020-05-20 18:38:46 +0200

    Create app.js

---
+++ assignments/assignment3/website/js/app.js
@@ -0,0 +1,76 @@
+const toggleNavigationIconHome = (navToggler) => {
+    const navLinks = document.querySelector('.nav-links');
+    const searchLinks = document.querySelector('.zoekbalk');
+    const searchLinksIcon = document.querySelector('.fa-search');
+    const row1Links = document.querySelector(".row1-links");
+    const row2Links = document.querySelector(".row2-links");
+    const row3Links = document.querySelector(".row3-links");
+    const footerLinks = document.querySelector('.footer-links');
+    const userIcon = document.querySelector('.user-icon');
+    const loginLinks = document.getElementById('login-text');
+    loginLinks.classList.toggle('toggled');
+    userIcon.classList.toggle('fa-user');
+    footerLinks.classList.toggle('toggled');
+    row1Links.classList.toggle('toggled');
+    row2Links.classList.toggle('toggled');
+    row3Links.classList.toggle('toggled');
+    searchLinksIcon.classList.toggle('toggled');
+    searchLinks.classList.toggle('toggled');
+    navLinks.classList.toggle('toggled');
+    navToggler.classList.toggle('toggled');
+}
+
+const toggleNavigationIconList = (navToggler) => {
+    const navLinks = document.querySelector('.nav-links');
+    const searchLinks = document.querySelector('.zoekbalk');
+    const searchLinksIcon = document.querySelector('.fa-search');
+    const row1Links = document.querySelector(".row1-links");
+    const footerLinks = document.querySelector('.footer-links');
+    const userIcon = document.querySelector('.user-icon');
+    const articleLinks = document.querySelector('.article-no');
+    articleLinks.classList.toggle('toggled');
+    document.getElementById('user-icon').innerHTML = "LOG IN";
+    userIcon.classList.remove('fa-user');
+    footerLinks.classList.toggle('toggled');
+    row1Links.classList.toggle('toggled');
+    searchLinksIcon.classList.toggle('toggled');
+    searchLinks.classList.toggle('toggled');
+    navLinks.classList.toggle('toggled');
+    navToggler.classList.toggle('toggled');
+}
+
+const toggleNavigationIconDetail = (navToggler) => {
+    const navLinks = document.querySelector('.nav-links');
+    const searchLinks = document.querySelector('.zoekbalk');
+    const searchLinksIcon = document.querySelector('.fa-search');
+    const mainLinks = document.querySelector('.no-detail');
+    const footerLinks = document.querySelector('.footer-links');
+    const userIcon = document.querySelector('.user-icon');
+    document.getElementById('user-icon').innerHTML = "LOG IN";
+    mainLinks.classList.toggle('toggled')
+    userIcon.classList.remove('fa-user');
+    footerLinks.classList.toggle('toggled');
+    searchLinksIcon.classList.toggle('toggled');
+    searchLinks.classList.toggle('toggled');
+    navLinks.classList.toggle('toggled');
+    navToggler.classList.toggle('toggled');
+}
+
+const toggleNavigationIconContact = (navToggler) => {
+    const navLinks = document.querySelector('.nav-links');
+    const searchLinks = document.querySelector('.zoekbalk');
+    const searchLinksIcon = document.querySelector('.fa-search');
+    const footerLinks = document.querySelector('.footer-links');
+    const userIcon = document.querySelector('.user-icon');
+    document.getElementById('user-icon').innerHTML = "LOG IN";
+    userIcon.classList.remove('fa-user');
+    footerLinks.classList.toggle('toggled');
+    searchLinksIcon.classList.toggle('toggled');
+    searchLinks.classList.toggle('toggled');
+    navLinks.classList.toggle('toggled');
+    navToggler.classList.toggle('toggled');
+}
+
+const toggleDropdownNavigation = (dropdownToggler) => {
+    dropdownToggler.classList.toggle('toggled');
+}
